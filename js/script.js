2c5c5b9685df14a40957c0ac9162d66e5244ca78
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-09-15 11:50:16 +0200

    Added js properties.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Jeg er guess number!");
+
+const lblMessage = document.querySelector(".message");
+console.log(lblMessage);
+console.log(lblMessage.textContent);
+
+const lblNumber = document.querySelector(".number");
+console.log(lblNumber);
+
+const lblScore = document.querySelector(".score");
+console.log(lblScore);
+
+const inpGuess = document.querySelector(".guess");
+
+
+out(inpGuess)
