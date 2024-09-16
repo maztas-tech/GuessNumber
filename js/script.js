3e71723dc720c5966b3b0189e496a7ffbf33ead8
maztas-tech <mata0001@stud.kea.dk>
2024-09-16 15:35:03 +0200

    It generates random number It check if the guess number and random number are equal to each other.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 console.log("Jeg er guess number!");
+//lbl står for label
 
 const lblMessage = document.querySelector(".message");
 console.log(lblMessage);
@@ -12,5 +13,31 @@ console.log(lblScore);
 
 const inpGuess = document.querySelector(".guess");
 
+const pbAgain = document.querySelector(".again");
 
-out(inpGuess)
+const pbGuess = document.querySelector(".check");
+
+let randomNumber = 0;
+
+
+
+
+//METODER
+//Generer random nummer
+function randomNumberGenerator(){
+    randomNumber = Math.trunc(Math.random() * 20) + 1;
+    lblNumber.textContent = randomNumber;
+}
+pbAgain.addEventListener("click", randomNumberGenerator);
+
+function guessNumber(){
+    const guess = inpGuess.value;
+    if (guess == randomNumber){
+        lblMessage.textContent = "du har vundet!";
+    }
+}
+
+//----------------------------------------------------------------------
+pbGuess.addEventListener("click", guessNumber);
+
+
